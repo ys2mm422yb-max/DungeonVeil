1081a8ebf2dbc00b9e88da447928a8a35a845236
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-07 20:10:51 +0200

    Add Tiny Swords terrain asset paths

---
+++ artifacts/dungeon-rpg/src/game/tinyWorldAssets.ts
@@ -0,0 +1,2 @@
+export const TINY_GROUND = '/assets/rpg-pack/Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Terrain/Tileset/Tilemap_color1.png';
+export const TINY_WATER = '/assets/rpg-pack/Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Terrain/Tileset/Water Background color.png';
