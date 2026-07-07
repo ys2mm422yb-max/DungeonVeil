54701f75d156321affb52c14d09c109c62e3605e
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-07 22:33:36 +0200

    Add pause panel imports

--- artifacts/dungeon-rpg/src/components/GamePausePanel.tsx
+++ artifacts/dungeon-rpg/src/components/GamePausePanel.tsx
@@ -1,1 +1,21 @@
-export {};
+import React from 'react';
+import { GameState } from '../game/engine';
+import { Language } from '../i18n/translations';
+
+export interface GamePausePanelProps {
+  gameState: GameState;
+  language: Language;
+  paused: string;
+  resume: string;
+  settings: string;
+  classNameText: string;
+  onResume: () => void;
+  onSave: () => void;
+  onSettings: () => void;
+  onMainMenu: () => void;
+  onLanguage: (language: Language) => void;
+}
+
+export function GamePausePanel(_props: GamePausePanelProps) {
+  return null;
+}
