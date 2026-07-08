37ba1b315e3b9a26a2e360646de4ceb20bc2e616
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-08 19:13:38 +0200

    Remove floating banners and retheme KayKit rooms

--- artifacts/dungeon-rpg/src/game/kaykitRoomLayout.ts
+++ artifacts/dungeon-rpg/src/game/kaykitRoomLayout.ts
@@ -5,9 +5,6 @@ export type KayKitRoomAsset =
   | 'corner'
   | 'wallColumn'
   | 'column'
-  | 'bannerRed'
-  | 'bannerBlue'
-  | 'bannerGreen'
   | 'barrel'
   | 'barrelDecorated'
   | 'barrelStack'
@@ -17,7 +14,12 @@ export type KayKitRoomAsset =
   | 'chestGold'
   | 'candle'
   | 'chair'
-  | 'bed';
+  | 'bed'
+  | 'torchMounted'
+  | 'wallShelves'
+  | 'swordShield'
+  | 'tableLong'
+  | 'tableMedium';
 
 export type KayKitRoomPlacement = {
   asset: KayKitRoomAsset;
@@ -29,87 +31,76 @@ export type KayKitRoomPlacement = {
 
 export const KAYKIT_ROOM_PROPS: Record<number, KayKitRoomPlacement[]> = {
   1: [
-    { asset: 'crates', x: -6.2, z: -5.8 }, { asset: 'barrelStack', x: -5.2, z: -4.4 },
-    { asset: 'barrelDecorated', x: 5.8, z: -5.4 }, { asset: 'boxLarge', x: 5.0, z: -3.8 },
-    { asset: 'bannerRed', x: -6.7, z: -1.3, rotation: Math.PI / 2 },
-    { asset: 'bannerRed', x: 6.7, z: -1.3, rotation: -Math.PI / 2 },
-    { asset: 'chest', x: 5.6, z: 4.7, rotation: Math.PI }, { asset: 'candle', x: -5.7, z: 4.8 },
+    { asset: 'crates', x: -5.8, z: -5.6 }, { asset: 'barrelStack', x: -4.7, z: -4.5 },
+    { asset: 'barrelDecorated', x: 5.7, z: -5.3 }, { asset: 'boxLarge', x: 4.8, z: -4.0 },
+    { asset: 'swordShield', x: -6.7, z: -0.8, rotation: Math.PI / 2 }, { asset: 'swordShield', x: 6.7, z: -0.8, rotation: -Math.PI / 2 },
+    { asset: 'torchMounted', x: -6.65, z: 3.0, rotation: Math.PI / 2 }, { asset: 'torchMounted', x: 6.65, z: 3.0, rotation: -Math.PI / 2 },
+    { asset: 'chest', x: 5.4, z: 5.0, rotation: Math.PI },
   ],
   2: [
     { asset: 'column', x: -4.8, z: -4.8 }, { asset: 'column', x: 4.8, z: -4.8 },
     { asset: 'column', x: -4.8, z: 3.8 }, { asset: 'column', x: 4.8, z: 3.8 },
-    { asset: 'bannerBlue', x: -6.7, z: -0.5, rotation: Math.PI / 2 },
-    { asset: 'bannerBlue', x: 6.7, z: -0.5, rotation: -Math.PI / 2 },
+    { asset: 'wallShelves', x: -6.55, z: -0.4, rotation: Math.PI / 2 }, { asset: 'wallShelves', x: 6.55, z: -0.4, rotation: -Math.PI / 2 },
+    { asset: 'torchMounted', x: -6.65, z: 4.1, rotation: Math.PI / 2 }, { asset: 'torchMounted', x: 6.65, z: 4.1, rotation: -Math.PI / 2 },
     { asset: 'chest', x: 0, z: 5.2, rotation: Math.PI },
   ],
   3: [
     { asset: 'barrel', x: -5.5, z: -5.6 }, { asset: 'barrel', x: -4.4, z: -5.0 },
     { asset: 'barrelDecorated', x: 5.4, z: -5.4 }, { asset: 'crates', x: 4.6, z: -3.8 },
-    { asset: 'candle', x: -5.8, z: 2.8 }, { asset: 'candle', x: 5.8, z: 2.8 },
+    { asset: 'tableMedium', x: -4.5, z: 3.9, rotation: Math.PI / 2 }, { asset: 'tableMedium', x: 4.5, z: 3.9, rotation: -Math.PI / 2 },
+    { asset: 'torchMounted', x: -6.65, z: 1.8, rotation: Math.PI / 2 }, { asset: 'torchMounted', x: 6.65, z: 1.8, rotation: -Math.PI / 2 },
     { asset: 'chestGold', x: 0, z: 5.3, rotation: Math.PI },
   ],
   4: [
-    { asset: 'bed', x: -5.3, z: -4.5, rotation: Math.PI / 2 },
-    { asset: 'bed', x: 5.3, z: -4.5, rotation: -Math.PI / 2 },
-    { asset: 'chair', x: -5.2, z: 2.2, rotation: Math.PI / 2 },
-    { asset: 'chair', x: 5.2, z: 2.2, rotation: -Math.PI / 2 },
-    { asset: 'bannerGreen', x: 0, z: -8.2, rotation: Math.PI },
+    { asset: 'bed', x: -5.3, z: -4.5, rotation: Math.PI / 2 }, { asset: 'bed', x: 5.3, z: -4.5, rotation: -Math.PI / 2 },
+    { asset: 'chair', x: -5.2, z: 2.2, rotation: Math.PI / 2 }, { asset: 'chair', x: 5.2, z: 2.2, rotation: -Math.PI / 2 },
+    { asset: 'tableLong', x: 0, z: -5.8 },
+    { asset: 'torchMounted', x: -3.2, z: -7.55, rotation: Math.PI }, { asset: 'torchMounted', x: 3.2, z: -7.55, rotation: Math.PI },
     { asset: 'chest', x: 0, z: 5.4, rotation: Math.PI },
   ],
   5: [
     { asset: 'column', x: -5.0, z: -5.3 }, { asset: 'column', x: 5.0, z: -5.3 },
     { asset: 'column', x: -5.0, z: 2.8 }, { asset: 'column', x: 5.0, z: 2.8 },
-    { asset: 'bannerRed', x: -2.0, z: -8.2, rotation: Math.PI },
-    { asset: 'bannerRed', x: 2.0, z: -8.2, rotation: Math.PI },
-    { asset: 'chestGold', x: 0, z: 5.2, rotation: Math.PI },
+    { asset: 'swordShield', x: -3.0, z: -7.6, rotation: Math.PI }, { asset: 'swordShield', x: 3.0, z: -7.6, rotation: Math.PI },
+    { asset: 'torchMounted', x: 0, z: -7.55, rotation: Math.PI }, { asset: 'chestGold', x: 0, z: 5.2, rotation: Math.PI },
   ],
   6: [
     { asset: 'crates', x: -5.3, z: -5.5 }, { asset: 'boxLarge', x: -4.2, z: -4.1 },
     { asset: 'barrelStack', x: 5.3, z: -5.4 }, { asset: 'barrel', x: 4.4, z: -4.0 },
-    { asset: 'crates', x: -5.2, z: 3.7 }, { asset: 'barrelDecorated', x: 5.1, z: 3.8 },
-    { asset: 'chest', x: 0, z: 5.2, rotation: Math.PI },
+    { asset: 'tableLong', x: -4.7, z: 3.8, rotation: Math.PI / 2 }, { asset: 'barrelDecorated', x: 5.1, z: 3.8 },
+    { asset: 'torchMounted', x: 0, z: -7.55, rotation: Math.PI }, { asset: 'chest', x: 0, z: 5.2, rotation: Math.PI },
   ],
   7: [
     { asset: 'column', x: -5.6, z: -5.4 }, { asset: 'column', x: 5.6, z: -5.4 },
-    { asset: 'bannerBlue', x: -6.7, z: -0.8, rotation: Math.PI / 2 },
-    { asset: 'bannerBlue', x: 6.7, z: -0.8, rotation: -Math.PI / 2 },
-    { asset: 'candle', x: -3.0, z: 4.7 }, { asset: 'candle', x: 3.0, z: 4.7 },
+    { asset: 'wallShelves', x: -6.55, z: -0.8, rotation: Math.PI / 2 }, { asset: 'wallShelves', x: 6.55, z: -0.8, rotation: -Math.PI / 2 },
+    { asset: 'torchMounted', x: -3.0, z: -7.55, rotation: Math.PI }, { asset: 'torchMounted', x: 3.0, z: -7.55, rotation: Math.PI },
     { asset: 'chestGold', x: 0, z: 5.4, rotation: Math.PI },
   ],
   8: [
-    { asset: 'bed', x: -5.4, z: -5.0, rotation: Math.PI / 2 },
-    { asset: 'chair', x: -5.2, z: -2.5, rotation: Math.PI / 2 },
+    { asset: 'bed', x: -5.4, z: -5.0, rotation: Math.PI / 2 }, { asset: 'chair', x: -5.2, z: -2.5, rotation: Math.PI / 2 },
     { asset: 'crates', x: 5.2, z: -5.1 }, { asset: 'barrelStack', x: 5.0, z: -3.4 },
-    { asset: 'bannerGreen', x: 0, z: -8.2, rotation: Math.PI },
+    { asset: 'tableMedium', x: 0, z: -5.8 },
+    { asset: 'torchMounted', x: -6.65, z: 2.8, rotation: Math.PI / 2 }, { asset: 'torchMounted', x: 6.65, z: 2.8, rotation: -Math.PI / 2 },
     { asset: 'chest', x: 0, z: 5.3, rotation: Math.PI },
   ],
   9: [
     { asset: 'column', x: -5.2, z: -4.5 }, { asset: 'column', x: 5.2, z: -4.5 },
     { asset: 'column', x: -5.2, z: 3.4 }, { asset: 'column', x: 5.2, z: 3.4 },
-    { asset: 'bannerRed', x: -2.3, z: -8.2, rotation: Math.PI },
-    { asset: 'bannerRed', x: 2.3, z: -8.2, rotation: Math.PI },
+    { asset: 'swordShield', x: -2.8, z: -7.6, rotation: Math.PI }, { asset: 'swordShield', x: 2.8, z: -7.6, rotation: Math.PI },
+    { asset: 'torchMounted', x: 0, z: -7.55, rotation: Math.PI },
     { asset: 'candle', x: -2.2, z: 5.0 }, { asset: 'candle', x: 2.2, z: 5.0 },
   ],
   10: [
     { asset: 'column', x: -5.5, z: -5.5, scale: 1.15 }, { asset: 'column', x: 5.5, z: -5.5, scale: 1.15 },
     { asset: 'column', x: -5.5, z: 3.8, scale: 1.15 }, { asset: 'column', x: 5.5, z: 3.8, scale: 1.15 },
-    { asset: 'bannerRed', x: -3.0, z: -8.2, rotation: Math.PI },
-    { asset: 'bannerRed', x: 0, z: -8.2, rotation: Math.PI },
-    { asset: 'bannerRed', x: 3.0, z: -8.2, rotation: Math.PI },
+    { asset: 'swordShield', x: -3.0, z: -7.6, rotation: Math.PI }, { asset: 'swordShield', x: 3.0, z: -7.6, rotation: Math.PI },
+    { asset: 'torchMounted', x: 0, z: -7.55, rotation: Math.PI },
     { asset: 'chestGold', x: 0, z: 5.4, rotation: Math.PI, scale: 1.2 },
   ],
 };
 
 export const KAYKIT_COLLISION_SIZE: Partial<Record<KayKitRoomAsset, [number, number]>> = {
-  column: [0.95, 0.95],
-  barrel: [0.9, 0.9],
-  barrelDecorated: [1.0, 1.0],
-  barrelStack: [1.75, 1.25],
-  crates: [1.9, 1.55],
-  boxLarge: [1.3, 1.3],
-  chest: [1.5, 1.05],
-  chestGold: [1.5, 1.05],
-  candle: [0.32, 0.32],
-  chair: [0.88, 0.88],
-  bed: [1.2, 2.1],
+  column: [0.95, 0.95], barrel: [0.9, 0.9], barrelDecorated: [1.0, 1.0], barrelStack: [1.75, 1.25],
+  crates: [1.9, 1.55], boxLarge: [1.3, 1.3], chest: [1.5, 1.05], chestGold: [1.5, 1.05],
+  candle: [0.32, 0.32], chair: [0.88, 0.88], bed: [1.2, 2.1], tableLong: [2.6, 1.05], tableMedium: [1.75, 1.2],
 };
