64ce3f79a5994eda539f35c27d2bb4b251ed343c
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-08 12:54:57 +0200

    Wrap 3D arena with combat feedback presentation

--- artifacts/dungeon-rpg/src/components/GameCanvas.tsx
+++ artifacts/dungeon-rpg/src/components/GameCanvas.tsx
@@ -1,1 +1,13 @@
-export { GameCanvasClearForest3D as GameCanvas } from './GameCanvasClearForest3D';
+import React from 'react';
+import type { GameState } from '../game/runEngine';
+import { GameCanvasClearForest3D } from './GameCanvasClearForest3D';
+import { CombatFeedbackOverlay } from './CombatFeedbackOverlay';
+
+export function GameCanvas({ gameState }: { gameState: GameState }) {
+  return (
+    <>
+      <GameCanvasClearForest3D gameState={gameState} />
+      <CombatFeedbackOverlay gameState={gameState} />
+    </>
+  );
+}
