01577fedebfd243b927900baf4ec7b455af0d888
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-08 13:01:44 +0200

    Activate themed vertical slice renderer

--- artifacts/dungeon-rpg/src/components/GameCanvas.tsx
+++ artifacts/dungeon-rpg/src/components/GameCanvas.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import type { GameState } from '../game/runEngine';
-import { GameCanvasClearForest3D } from './GameCanvasClearForest3D';
+import { GameCanvasVerticalSlice3D } from './GameCanvasVerticalSlice3D';
 import { CombatFeedbackOverlay } from './CombatFeedbackOverlay';
 
 export function GameCanvas({ gameState }: { gameState: GameState }) {
   return (
     <>
-      <GameCanvasClearForest3D gameState={gameState} />
+      <GameCanvasVerticalSlice3D gameState={gameState} />
       <CombatFeedbackOverlay gameState={gameState} />
     </>
   );
