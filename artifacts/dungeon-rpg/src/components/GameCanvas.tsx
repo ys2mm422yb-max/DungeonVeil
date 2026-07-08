6f14de99283218491a7d1e413647ff518494f27f
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-08 17:01:07 +0200

    Activate complete KayKit run renderer

--- artifacts/dungeon-rpg/src/components/GameCanvas.tsx
+++ artifacts/dungeon-rpg/src/components/GameCanvas.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import type { GameState } from '../game/runEngine';
-import { GameCanvasVerticalSlice3D } from './GameCanvasVerticalSlice3D';
+import { GameCanvasKayKit3D } from './GameCanvasKayKit3D';
 import { CombatFeedbackOverlay } from './CombatFeedbackOverlay';
 
 export function GameCanvas({ gameState }: { gameState: GameState }) {
   return (
     <>
-      <GameCanvasVerticalSlice3D gameState={gameState} />
+      <GameCanvasKayKit3D gameState={gameState} />
       <CombatFeedbackOverlay gameState={gameState} />
     </>
   );
