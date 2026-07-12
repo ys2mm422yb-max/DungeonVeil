62e9dd659d203d6389193fabbab63e9bae9be676
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-12 20:07:13 +0200

    Start deployed build version guard

--- artifacts/dungeon-rpg/src/main.tsx
+++ artifacts/dungeon-rpg/src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { startVersionGuard } from './game/versionGuard';
 
 import './index.css';
 
+startVersionGuard();
 createRoot(document.getElementById('root')!).render(<App />);
