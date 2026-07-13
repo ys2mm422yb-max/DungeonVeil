6e6dbdc09404c15c08063a882c32691764141ab4
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-13 06:10:50 +0200

    Add deterministic village menu visual QA view

---
+++ artifacts/dungeon-rpg/src/components/MainMenuVisualQa.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { LanguageProvider, useLanguage } from '../i18n/LanguageContext';
+import { MainMenuScreen } from './screens/MainMenuScreen';
+
+function MenuQaContent() {
+  const { setLanguage } = useLanguage();
+  useEffect(() => { setLanguage('de'); }, [setLanguage]);
+
+  return <MainMenuScreen
+    saveData={null}
+    onNewGame={() => {}}
+    onContinue={() => {}}
+    onVeilChamber={() => {}}
+    onCodex={() => {}}
+    onSettings={() => {}}
+    onCredits={() => {}}
+  />;
+}
+
+export function MainMenuVisualQa() {
+  return <LanguageProvider><MenuQaContent /></LanguageProvider>;
+}
