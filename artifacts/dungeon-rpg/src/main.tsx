5bcde1bf3b8c24642e4bf37b3166f0973c07c4c8
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-13 06:12:01 +0200

    Expose deterministic village menu visual QA route

--- artifacts/dungeon-rpg/src/main.tsx
+++ artifacts/dungeon-rpg/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { MainMenuVisualQa } from './components/MainMenuVisualQa';
 import { TutorialVisualQa } from './components/TutorialVisualQa';
 import { WorldBossVisualQa } from './components/WorldBossVisualQa';
 import { startVersionGuard } from './game/versionGuard';
@@ -9,5 +10,11 @@ import './index.css';
 
 const qaMode = typeof window !== 'undefined' ? new URLSearchParams(window.location.search).get('qa') : null;
 if (!qaMode) startVersionGuard();
-const qaView = qaMode === 'worldboss' ? <WorldBossVisualQa /> : qaMode === 'tutorial' ? <TutorialVisualQa /> : <App />;
+const qaView = qaMode === 'worldboss'
+  ? <WorldBossVisualQa />
+  : qaMode === 'tutorial'
+    ? <TutorialVisualQa />
+    : qaMode === 'menu'
+      ? <MainMenuVisualQa />
+      : <App />;
 createRoot(document.getElementById('root')!).render(qaView);
