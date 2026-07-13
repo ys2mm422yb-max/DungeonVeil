cb5a700cfda0274ad4d5087f5a6794f7df0a5613
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-13 04:02:02 +0200

    Expose world boss visual QA route

--- artifacts/dungeon-rpg/src/main.tsx
+++ artifacts/dungeon-rpg/src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { WorldBossVisualQa } from './components/WorldBossVisualQa';
 import { startVersionGuard } from './game/versionGuard';
 
 import './index.css';
 
-startVersionGuard();
-createRoot(document.getElementById('root')!).render(<App />);
+const qaMode = typeof window !== 'undefined' ? new URLSearchParams(window.location.search).get('qa') : null;
+if (!qaMode) startVersionGuard();
+createRoot(document.getElementById('root')!).render(qaMode === 'worldboss' ? <WorldBossVisualQa /> : <App />);
