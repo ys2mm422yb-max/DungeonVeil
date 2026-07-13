6de36aa8559490a67976e561140da749c9cf1b48
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-14 00:12:08 +0200

    Install signup redirect guard before app startup

--- artifacts/dungeon-rpg/src/main.tsx
+++ artifacts/dungeon-rpg/src/main.tsx
@@ -4,10 +4,13 @@ import App from './App';
 import { MainMenuVisualQa } from './components/MainMenuVisualQa';
 import { TutorialVisualQa } from './components/TutorialVisualQa';
 import { WorldBossVisualQa } from './components/WorldBossVisualQa';
+import { installEmailConfirmationRedirect } from './game/emailConfirmationRedirect';
 import { startVersionGuard } from './game/versionGuard';
 
 import './index.css';
 import './guild-mobile.css';
+
+installEmailConfirmationRedirect();
 
 const qaMode = typeof window !== 'undefined' ? new URLSearchParams(window.location.search).get('qa') : null;
 if (!qaMode) startVersionGuard();
