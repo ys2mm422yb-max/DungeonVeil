7aa5a0691ed9d408e07522c6c4b33ca5cf656aab
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-15 18:41:48 +0200

    Mount unlock presentation layer

--- artifacts/dungeon-rpg/src/main.tsx
+++ artifacts/dungeon-rpg/src/main.tsx
@@ -6,6 +6,7 @@ import App from './App';
 import { GlobalLoadingLayer } from './components/GlobalLoadingLayer';
 import { MainMenuVisualQa } from './components/MainMenuVisualQa';
 import { TutorialVisualQa } from './components/TutorialVisualQa';
+import { UnlockPresentationLayer } from './components/UnlockPresentationLayer';
 import { WorldBossVisualQa } from './components/WorldBossVisualQa';
 import { installEmailConfirmationRedirect } from './game/emailConfirmationRedirect';
 import { startVersionGuard } from './game/versionGuard';
@@ -24,5 +25,5 @@ const qaView = qaMode === 'worldboss'
     : qaMode === 'menu'
       ? <MainMenuVisualQa />
       : null;
-const appView = qaView ?? <><App /><GlobalLoadingLayer /></>;
+const appView = qaView ?? <><App /><GlobalLoadingLayer /><UnlockPresentationLayer /></>;
 createRoot(document.getElementById('root')!).render(appView);
