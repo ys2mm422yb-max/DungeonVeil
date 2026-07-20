06376b2581ef3120a360e89937cb0a8d49b48e67
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-07-20 09:50:27 +0200

    Compose hybrid Hall background with live 3D scene

--- artifacts/dungeon-rpg/src/components/ModernVillageSquareScene.tsx
+++ artifacts/dungeon-rpg/src/components/ModernVillageSquareScene.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HallOfVeilHybridBackground } from './HallOfVeilHybridBackground';
 import { HallOfVeilScene } from './HallOfVeilScene';
 
 /**
@@ -6,5 +7,10 @@ import { HallOfVeilScene } from './HallOfVeilScene';
  * The former market square has been fully replaced by the Hall of the Veil.
  */
 export function ModernVillageSquareScene() {
-  return <HallOfVeilScene />;
+  return <div className="pointer-events-none absolute inset-0 overflow-hidden" data-hall-hybrid-composition="true">
+    <HallOfVeilHybridBackground />
+    <div className="absolute inset-0 opacity-[0.88] mix-blend-screen">
+      <HallOfVeilScene />
+    </div>
+  </div>;
 }
